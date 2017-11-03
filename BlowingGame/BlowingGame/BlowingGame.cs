ba83079ac25afc83a015bfa8644282919c59837e
TITANSOFT\jack.hsu71 <ace05152535@gmail.com>
2017-11-03 17:44:50 +0800

    first commit

---
+++ BlowingGame/BlowingGame/BlowingGame.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlowingGame
+{
+    class BlowingGame
+    {
+        public int add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Round> parseToRound(string input)
+        {
+            var stringArray = input.Split(',');
+            List<Round> rounds = new List<Round>();
+            for (int i = 0; i < stringArray.Length; i+=2)
+            {
+                var round = new Round();
+                round.first = int.Parse(stringArray[i]);
+                round.second = int.Parse(stringArray[i+1]);
+                rounds.Add(round);
+            }         
+            return rounds;
+        }
+
+
+        public bool isStrike(Round round)
+        {
+            if (round.first == 10)
+                return true;
+            return false;
+        }
+
+   
+    }
+
+    class Round
+    {
+        public int first;
+        public int second;
+    }
+}
---
+++ BlowingGame/BlowingGame/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace BlowingGame
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void input()
+        {
+            string input = "1,1,1,1";
+            List<Round> actual = new List<Round>();
+            List<Round> expected = new List<Round>();
+
+            BlowingGame game = new BlowingGame();
+            actual = game.parseToRound(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void isStrike()
+        {                       
+            BlowingGame game = new BlowingGame();
+            var round = new Round();
+            round.first = 10;
+            round.second = 0;
+            bool actual = game.isStrike(round);
+            bool expected = true;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void isSpare()
+        {
+            BlowingGame game = new BlowingGame();
+            bool actual = game.isSpare();
+            bool expected = true;
+            Assert.AreEqual(expected, actual);
+        }
+
+
+    }
+}
